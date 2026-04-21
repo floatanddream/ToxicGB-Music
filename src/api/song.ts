4811677abd5bc52e365bf59f7a6fd794770866d3
ToxicGB <2501509140@qq.com>
2026-04-21 17:00:21 +0800

    修复由于后端缓存机制导致歌曲链接不正确的bug

--- src/api/song.ts
+++ src/api/song.ts
@@ -1,10 +1,12 @@
 import request from '@/utils/request'
+import { timestamp } from '@vueuse/core';
 
 //获取歌手详情
 export const getSongUrl = async (id: number | string) => {
-    return await request.post(`/song/url/v1`,{
+    return await request.get(`/song/url/v1`,{
         id,
         level:'exhigh',
         sendCookie:true,
+        timestamp:new Date().getTime(),
     });
 }
