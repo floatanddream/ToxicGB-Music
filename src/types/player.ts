896b45093adec69618024d687823867f335139c1
ToxicGB <2501509140@qq.com>
2026-04-21 14:04:52 +0800

    1

--- src/types/player.ts
+++ src/types/player.ts
@@ -3,7 +3,7 @@ import type * as MusicType from '@/types/musicTypes';
 export type PlayMode = 'loop' | 'single' | 'random';
 
 export interface Song extends MusicType.Song {
-  url: string
+  url?: string
 };
 
 export type SongLoader = (id: number | string) => Promise<Song>
