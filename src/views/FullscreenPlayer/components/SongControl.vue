27e44c67a8c0f635a89233605d76242c270b3b25
ToxicGB <2501509140@qq.com>
2026-05-16 17:20:48 +0800

    格式化代码

--- src/views/FullscreenPlayer/components/SongControl.vue
+++ src/views/FullscreenPlayer/components/SongControl.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BouncingSlider from '@/components/misc/BouncingSlider.vue'
+import { formatTime } from '@/utils/format';
 
 const props = defineProps<{
   duration: number
@@ -18,15 +19,15 @@ const handleSeek = (time: number) => {
 
 <template>
   <div class="song-control">
-    <BouncingSlider
-      :value="currentTime"
-      :min="0"
-      :max="duration"
-      :is-playing="false"
-      :change-on-drag="true"
-      @change="handleSeek"
-      @after-change="handleSeek"
-    />
+    <div class="progressBar">
+      <BouncingSlider :value="currentTime" :min="0" :max="duration" :is-playing="false" :change-on-drag="true"
+        @change="handleSeek" @after-change="handleSeek" />
+      <div class="timeDisplay flex justify-between">
+        <div class="totalTime">{{ formatTime(duration) }}</div>
+        <div class="leftTime"> - {{ formatTime(duration - currentTime) }}</div>
+      </div>
+    </div>
+
   </div>
 </template>
 
