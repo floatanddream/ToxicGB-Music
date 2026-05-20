49bd23cdd5470e717a837f2ebc08437cb4e9f129
ToxicGB <2501509140@qq.com>
2026-05-20 13:53:54 +0800

    实现播放器歌曲控制

--- src/views/FullscreenPlayer/components/SongControl.vue
+++ src/views/FullscreenPlayer/components/SongControl.vue
@@ -3,6 +3,8 @@ import BouncingSlider from '@/components/misc/BouncingSlider.vue'
 import { formatTime } from '@/utils/format'
 import { usePlayerStore } from '@/stores/playerStore'
 import ArtistDivider from '@/components/common/musicComponents/artistDivider.vue'
+import { FastForward, NotepadTextDashedIcon, PauseIcon, PlayIcon, Rewind } from 'lucide-vue-next'
+import { reactive } from 'vue'
 
 const playerStore = usePlayerStore()
 
@@ -11,6 +13,12 @@ const props = defineProps<{
   currentTime: number
   isPlaying: boolean
 }>()
+
+const songBtn = reactive({
+  prev: false,
+  play: false,
+  next: false
+})
 
 const emit = defineEmits<{
   seek: [time: number]
@@ -28,28 +36,34 @@ const handleSeek = (time: number) => {
       <ArtistDivider :artists="playerStore.currentSong?.artist || []" />
     </div>
     <div class="progressBar">
-      <BouncingSlider
-        :value="currentTime"
-        :min="0"
-        :max="duration"
-        :is-playing="false"
-        :change-on-drag="true"
-        @change="handleSeek"
-        @after-change="handleSeek"
-      />
+      <BouncingSlider :value="currentTime" :min="0" :max="duration" :is-playing="false" :change-on-drag="true"
+        @change="handleSeek" @after-change="handleSeek" />
       <div class="timeDisplay flex justify-between">
         <div class="totalTime">{{ formatTime(duration) }}</div>
         <div class="leftTime">- {{ formatTime(duration - currentTime) }}</div>
       </div>
-      <div class="control flex justify-center gap-10 mt-10">
-        <div class="prevSong" @click="playerStore.next()">上一首</div>
-        <div
-          class="playPause"
-          @click="playerStore.playing ? playerStore.pause() : playerStore.play()"
-        >
-          播放/暂停
-        </div>
-        <div class="nextSong" @click="playerStore.next()">下一歌曲</div>
+    </div>
+    <div class="control flex justify-center gap-10 mt-5">
+      <div class="flex justify-center items-center h-15 w-15 cursor-pointer prevSong
+           rounded-full hover:bg-[rgba(0,0,0,0.08)] transition-colors duration-200" @click="playerStore.next()"
+        @mousedown="songBtn.prev = true" @mouseup="songBtn.prev = false">
+        <Rewind :style="{ width: songBtn.prev ? '2rem' : '2.5rem', height: songBtn.prev ? '2rem' : '2.5rem' }" />
+      </div>
+
+      <div class="flex justify-center items-center h-15 w-15 cursor-pointer playPause
+           rounded-full hover:bg-[rgba(0,0,0,0.08)] transition-colors duration-200"
+        @click="playerStore.playing ? playerStore.pause() : playerStore.play()" @mousedown="songBtn.play = true"
+        @mouseup="songBtn.play = false">
+        <PauseIcon :style="{ width: songBtn.play ? '2rem' : '2.5rem', height: songBtn.play ? '2rem' : '2.5rem' }"
+          v-if="playerStore.playing" />
+        <PlayIcon :style="{ width: songBtn.play ? '2rem' : '2.5rem', height: songBtn.play ? '2rem' : '2.5rem' }"
+          v-else />
+      </div>
+
+      <div class="flex justify-center items-center h-15 w-15 cursor-pointer nextSong
+           rounded-full hover:bg-[rgba(0,0,0,0.08)] transition-colors duration-200" @click="playerStore.next()"
+        @mousedown="songBtn.next = true" @mouseup="songBtn.next = false">
+        <FastForward :style="{ width: songBtn.next ? '2rem' : '2.5rem', height: songBtn.next ? '2rem' : '2.5rem' }" />
       </div>
     </div>
   </div>
